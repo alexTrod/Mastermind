0d048413e7a4163a071c958bd3d30236f8c49db1
RowdeyGoos <45183362+RowdeyGoos@users.noreply.github.com>
2018-12-16 16:43:44 +0100

    Update statTracker.js

--- statTracker.js
+++ statTracker.js
@@ -2,11 +2,11 @@
  In-memory game statistics "tracker".
 */
 
-var gameStats = {
+var gameStatus = {
     since : Date.now(),     /* since we keep it simple and in-memory, keep track of when this object was created */
     gamesWonCM : 0,   /* number of games won as codemaker */
     gamesWONCB : 0,       /* number of games won as codebreaker */
-    gamesTime : 0      /* Time spent playing the game */
+    gamesInitialized : 0      /* Time spent playing the game */
 };
 
 module.exports = gameStats;
