d1845f103b924ac99067fcefb7403374ea54f0ad
Alexandra Feldman <45596097+alexTrod@users.noreply.github.com>
2018-12-17 14:47:43 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/game', function(req, res){
+    res.sendFile("game.html", {root: "./public"});
+});
+
 module.exports = router;
